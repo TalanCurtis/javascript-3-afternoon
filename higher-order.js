3109520c3e8a363aa42c463ee032f7758da6ae1f
Alan Curtis <talancurtis@gmail.com>
2018-01-11 00:12:33 -0700

    highger order completted other than, problem 5

--- higher-order.js
+++ higher-order.js
@@ -20,8 +20,8 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+
+let evenNumbers =mixedNumbers.filter((x)=> x%2===0)
 
 
 
@@ -44,7 +44,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices  = prices.map(x => x*1.07);
 
 
 
@@ -63,8 +63,8 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
+let totalPopulation   = populations.reduce((acc, x)=>acc+x)
+totalPopulation
 
 
 ////////// PROBLEM 4 //////////
@@ -89,7 +89,8 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest  = monstersInYourPocket.filter(x => x.CP>200 )
+myStrongest
 
 
 
@@ -106,7 +107,12 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a high order method to get sum of all the order totals
 */
 
-let ordersTotal //Code Here
+let ordersTotal = orders.map((x)=>(Number(x.price)+ Number(x.tax)))
+// let ordersTotal = orders.reduce(function (previous, current) {
+//   return previous + current.price+ current.tax;
+// }, 0);
+ordersTotal
+
 
 
 
@@ -126,6 +132,12 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+// let bobsTotal = purchases.reduce(function (acc, x) {
+//   return acc + x.price;
+// }, 0);
+//.reduce((acc, x)=> {return acc + x.price;))
+let bobsTotal = purchases.filter(x=> x.owner ==="Bob")
+bobsTotal = bobsTotal.reduce((acc, x)=> {return acc+  x.price}, 0)
+bobsTotal
 
 
